ee7f750faaffe79e21d69dba22c27ddc14c27a66
Dan <danyi1212@gmail.com>
2021-01-30 01:00:45 +0200

    🐣 init

---
+++ drf_messages/__init__.py
@@ -0,0 +1,4 @@
+import logging
+
+logger = logging.getLogger("drf_messages")
+default_app_config = "drf_messages.apps.DrfMessagesConfig"
---
+++ drf_messages/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DrfMessagesConfig(AppConfig):
+    name = 'drf_messages'
