6af8cd6721ae2363c9224b588ba50ac42949c4e8
Dan Yishai <danyi1212@gmail.com>
2021-11-09 12:02:37 +0200

    Improved tests

--- testproj/demo/tests.py
+++ testproj/demo/tests.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from drf_messages.conf import MESSAGES_USE_SESSIONS, MESSAGES_DELETE_READ
 from drf_messages.models import Message
 
 
@@ -22,22 +21,22 @@ class MessageDRFViewsTests(APITestCase):
         self.login_session()
         # create message
         response = self.client.get(reverse('demo:index'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
         # read message
         response = self.client.get(reverse("drf_messages:messages-list"))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
         self.assertEqual(len(response.data.get("results")), 1)
         # should delete previous message
         response = self.client.get(reverse("drf_messages:messages-list"))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data.get("results")), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        self.assertEqual(len(response.data.get("results")), 0)
 
     def test_session_clear(self):
         self.login_session()
         # create message
         response = self.client.get(reverse('demo:index'))
         session_key = response.wsgi_request.session.session_key
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
         self.assertEqual(Message.objects.filter(session__session_key=session_key).count(), 1)
         # logout, clear session
         self.client.logout()
@@ -45,16 +44,15 @@ class MessageDRFViewsTests(APITestCase):
 
     @override_settings(MESSAGES_USE_SESSIONS=True)
     def test_multi_session(self):
-        # TODO settings not updating in conf.py
         self.login_session()
         # create message
         response = self.client.get(reverse('demo:index'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
         session_key = response.wsgi_request.session.session_key
         self.assertEqual(Message.objects.filter(session__session_key=session_key).count(), 1)
         # read message
         response = self.client.get(reverse("drf_messages:messages-list"))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
         self.assertEqual(len(response.data.get("results")), 1)
         # create new session
         new_client = Client()
@@ -63,11 +61,10 @@ class MessageDRFViewsTests(APITestCase):
         response = new_client.get(reverse("drf_messages:messages-list"))
         session_key = response.wsgi_request.session.session_key
         self.assertEqual(Message.objects.filter(session__session_key=session_key).count(), 0)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
         self.assertEqual(response.data.get("results"), [])
 
-    def test_leave_message(self):
-        text = "Leave a message"
+    def test_leave_message(self, text="Leave a message"):
         Message.objects.create_user_message(self.user, text, messages.INFO)
         self.login_session()
         response = self.client.get(reverse("drf_messages:messages-list"))
@@ -76,22 +73,25 @@ class MessageDRFViewsTests(APITestCase):
     def test_peak_messages(self):
         self.login_session()
         # create message
-        response = self.client.get(reverse('demo:index'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        Message.objects.create_user_message(self.user, "info message", messages.INFO)
         # peek messages
         response = self.client.get(reverse('drf_messages:messages-peek'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data.get("count"), 1)
-        self.assertEqual(response.data.get("max_level"), 20)
-        self.assertEqual(response.data.get("max_level_tag"), "info")
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        self.assertEqual(response.data, dict(
+            count=1,
+            max_level=20,
+            max_level_tag="info",
+        ))
         # create higher level message
-        Message.objects.create_user_message(self.user, "", messages.WARNING)
+        Message.objects.create_user_message(self.user, "warning message", messages.WARNING)
         # peek again
         response = self.client.get(reverse('drf_messages:messages-peek'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data.get("count"), 2)
-        self.assertEqual(response.data.get("max_level"), 30)
-        self.assertEqual(response.data.get("max_level_tag"), "warning")
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.data)
+        self.assertEqual(response.data, dict(
+            count=2,
+            max_level=30,
+            max_level_tag="warning",
+        ))
 
 
 class MessageTests(TestCase):
